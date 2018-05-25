edc8d91d200e639ea421b10b1726a56c94ed6ba7
BantaGaming <lucaunderwood12@gmail.com>
2018-05-25 14:28:27 +0100

    changes

--- src/main/java/com/spigotcodingacademy/ironman/listeners/j/a/r/v/i/s/PlayerLowHealth.java
+++ src/main/java/com/spigotcodingacademy/ironman/listeners/j/a/r/v/i/s/PlayerLowHealth.java
@@ -1,5 +1,6 @@
 package com.spigotcodingacademy.ironman.listeners.j.a.r.v.i.s;
 
+import com.spigotcodingacademy.ironman.Main;
 import com.spigotcodingacademy.ironman.manager.Data;
 import com.spigotcodingacademy.ironman.utils.Chat;
 import org.bukkit.entity.Player;
@@ -8,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class PlayerLowHealth implements Listener {
 
@@ -18,12 +20,18 @@ public class PlayerLowHealth implements Listener {
             if (Data.Suit.contains(player)) {
                 if (player.getHealth() < 10 && !Data.isLowHealth.contains(player)) {
                     Data.isLowHealth.add(player);
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 0));
                     Chat.msg(
                             player,
                             Chat.jarvis + "&cWarning! High damage sustained!",
                             Chat.jarvis + "&6Adrenaline has been administered."
                     );
+                    new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, Integer.MAX_VALUE, 1));
+                            player.sendMessage("added potion effect");
+                        }
+                    }.runTaskLater(Main.getPlugin(), 2*20);
                 }
             }
         }

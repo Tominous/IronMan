be09c4feb5383d4dbabb172f724c11fbdbbb7cd7
BlitzCentral <skype01702@gmail.com>
2019-04-15 22:34:27 +0100

    Added hashmap to data class

--- src/main/java/com/spigotcodingacademy/ironman/manager/Data.java
+++ src/main/java/com/spigotcodingacademy/ironman/manager/Data.java
@@ -3,7 +3,7 @@ package com.spigotcodingacademy.ironman.manager;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 public class Data {
 
@@ -12,4 +12,6 @@ public class Data {
     public static ArrayList<Player> isLowHealth = new ArrayList<>();
     public static ArrayList<Player> isOnFire = new ArrayList<>();
     public static ArrayList<Player> isPoisened = new ArrayList<>();
+    public static HashMap<Player, String> suitAssigned = new HashMap<>();
+
 }

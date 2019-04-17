f95e645a12df729575784c8490ebaae8e808bb9a
BlitzCentral <skype01702@gmail.com>
2019-04-17 14:03:22 +0100

    Removed op join message.

--- src/main/java/com/spigotcodingacademy/ironman/listeners/PlayerJoin.java
+++ src/main/java/com/spigotcodingacademy/ironman/listeners/PlayerJoin.java
@@ -1,6 +1,5 @@
 package com.spigotcodingacademy.ironman.listeners;
 
-import com.spigotcodingacademy.ironman.utils.Chat;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,9 +11,5 @@ public class PlayerJoin implements Listener{
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
 
-        if (player.isOp()) {
-            Chat.msg(player, Chat.prefix + Chat.color("IronMan is loaded!"));
-            Chat.msg(player, Chat.prefix + Chat.color("IronMan version: " + Chat.version()));
-        }
     }
 }
